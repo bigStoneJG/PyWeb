16809821b008b476b77f3cf6a6967c3633fb5a21
bigStoneJG <bigStoneJG@users.noreply.github.com>
2017-11-01 13:22:29 +0800

    first web

---
+++ PyWeb.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+import BaseHTTPServer
+
+class RequestHander(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    Page = '''\
+    <html>
+    <body>
+    <p>Hello,web!</p>
+    <p>Hello,Python!</p>
+    </body>
+    </html>
+    '''
+
+    def do_GET(self):
+        try:
+            f = open(self.path[1:], 'r')
+            self.send_response(200)
+            self.send_header("Content-Type","text/html")
+            #self.send_header("Content-Length",str(len(self.Page)))
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+        except IOError:
+            self.send_error(404,'file not found:%s' %self.path)
+
+if __name__ == '__main__':
+    serverAddress = ('',8080)
+    server = BaseHTTPServer.HTTPServer(serverAddress,RequestHander)
+    server.serve_forever()
